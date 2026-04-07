1cae926d8e710125ed90f413ee5cc1ca632983da
Geethika-92 <geethikatalari927@gmail.com>
2026-04-07 18:38:24 +0530

    Train Consist UC1 added

---
+++ src/TrainApp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TrainApp {
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> train = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + train.size());
+
+        System.out.println("Train initialized successfully.");
+    }
+}
